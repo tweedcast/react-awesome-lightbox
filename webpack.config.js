4b41828c766db21fe4d8104252b4f28c8c2a5a63
Harley Wegman <harley@tweedcast.com>
2022-08-29 23:04:29 -0400

    dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ var path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
-        libraryTarget: 'commonjs2' 
+        libraryTarget: 'commonjs2'
     },
+    mode: 'production',
     module: {
         rules: [
             {
